eed09ee4772effa965264a90b0180f2a87affa21
juhee67 <55428174+juhee67@users.noreply.github.com>
2020-11-12 15:58:57 +0900

    4

--- main.c
+++ main.c
@@ -3,16 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i = 100;
-	int *p = &i;
-	int **q = &p;
+char *proverb = "All that glisters is not gold.";
+void setPointer(char **q)
+{
+	*q = proverb;
+}
 
-	*p = 200;
-	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+int main(int argc, char *argv[]) {
 	
-	**q = 300;
-	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+	char *p = "zzz";
+	setPointer(&p);
+	printf("%s \n", p);
 	
 	return 0;
 }
